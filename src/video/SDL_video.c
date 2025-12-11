866af03994ef7b368e403477c51e21b67ab167a0
Brenton Bostick <bostick@gmail.com>
2025-12-11 10:59:22 -0500

    Synapse change: set swapinterval to 1

--- src/video/SDL_video.c
+++ src/video/SDL_video.c
@@ -5591,7 +5591,8 @@ bool SDL_GL_GetSwapInterval(int *interval)
        return SDL_InvalidParamError("interval");
     }
 
-    *interval = 0;
+    // BKB: change this to 1
+    *interval = 1;
 
     if (!_this) {
         return SDL_SetError("no video driver");
