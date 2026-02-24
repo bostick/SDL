9d957b3984643f302881acdc79e5f73708ef254a
Brenton Bostick <bostick@gmail.com>
2026-02-24 11:24:30 -0500

    SDL fixes: extra ;

--- src/dialog/haiku/SDL_haikudialog.cc
+++ src/dialog/haiku/SDL_haikudialog.cc
@@ -219,7 +219,7 @@ void SDL_SYS_ShowFileDialogWithProperties(SDL_FileDialogType type, SDL_DialogFil
     case SDL_FILEDIALOG_OPENFOLDER:
         folder = true;
         break;
-    };
+    }
 
     if (!SDL_InitBeApp()) {
         SDL_SetError("Couldn't init Be app: %s", SDL_GetError());
